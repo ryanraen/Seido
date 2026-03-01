6caa842b4a31961e9264eb901722a89a9bce6f34
mbuiiiz <ductai2068@gmail.com>
2026-03-01 10:21:23 -0800

    path

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Session from "./pages/Session";
-import ExerciseGallery from "./pages/ExerciseGallery";
+import Index from "@/pages/Index";
+import Session from "@/pages/Session";
+import ExerciseGallery from "@/pages/ExerciseGallery";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
